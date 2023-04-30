26bbce68f2bc993147ec4e57807ec230199480f1
unknown <scottymitch88@gmail.com>
2023-04-30 17:40:13 -0400

    Fix build

--- apps/oeth-dapp/tailwind.config.js
+++ apps/oeth-dapp/tailwind.config.js
@@ -1,8 +1,10 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 const { join } = require('path');
+const sharedTailwindConfig = require('../../packages/tailwind-preset/tailwind.config');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [sharedTailwindConfig],
   content: [
     join(
       __dirname,
@@ -10,36 +12,4 @@ module.exports = {
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        invisible: 'rgba(1,1,1,0)',
-        current: 'currentColor',
-        'origin-bg-black': '#101113',
-        'origin-bg-blackt': '#101113cc',
-        'origin-bg-dgrey': '#141519',
-        'origin-bg-dgreyt': '#141519e6',
-        'origin-bg-grey': '#18191C',
-        'origin-bg-greyt': '#18191Cb3',
-        'origin-bg-lgrey': '#1E1F25',
-        'origin-white': '#FAFBFB',
-        'gradient1-from': '#B361E6',
-        'gradient1-to': '#6A36FC',
-        'gradient2-from': '#8C66FC',
-        'gradient2-to': '#0274F1',
-        'gradient3-from': '#66FE90',
-        'gradient3-to': '#66D9FE',
-        'origin-dimmed': '#828699',
-        'origin-border': '#141519',
-        'origin-blue': '#0074f0',
-        'origin-secondary': '#FFDC86',
-      },
-      fontFamily: {
-        primary: ['Inter', 'sans-serif'],
-        header: ['Sailec', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
 };
