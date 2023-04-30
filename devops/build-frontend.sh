e6f6425504894d89c8f91701af7350a7e1126d5a
unknown <scottymitch88@gmail.com>
2023-04-30 15:53:15 -0400

    Add localized package.json and build

--- devops/build-frontend.sh
+++ devops/build-frontend.sh
@@ -1,21 +1,3 @@
 #!/bin/sh
-
-buildContracts() {
-  if [[ $APP_ID == "ousd-dapp" || $APP_ID == "oeth-dapp" ]]
-  then
-    echo "Building contract for the DApp";
-    cd contracts;
-    NODE_ENV=development yarn install;
-    yarn run deploy;
-    cd --;
-    cp -R dapp/abis apps/$APP_ID/abis
-    rm -rf contracts/node_modules # Purge
-  fi
-}
-
-buildApp() {
-  # buildContracts;
-  npx nx build $APP_ID --prod
-}
-
-buildApp
+yarn install --frozen-lockfile
+npx nx build $APP_ID --prod
